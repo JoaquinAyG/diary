be29a77be9ccb2addfcd02aff0aacf321229df26
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2022-11-19 13:45:53 +0100

    main activity

--- app/src/main/java/personal/study/diary/MainActivity.kt
+++ app/src/main/java/personal/study/diary/MainActivity.kt
@@ -2,10 +2,24 @@ package personal.study.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import personal.study.diary.adapters.ContactListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvContacts)
+        val butAdd = findViewById<ImageButton>(R.id.butAdd)
+        val adapter = ContactListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
     }
 }
